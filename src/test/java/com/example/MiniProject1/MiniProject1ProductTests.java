f7b61ec46ae455727dce0a16f50cdf6a5715d6f9
Nadaelbehery <120909884+Nadaelbehery@users.noreply.github.com>
2025-03-01 23:37:58 +0200

    init product tests

--- src/test/java/com/example/MiniProject1/MiniProject1ProductTests.java
+++ src/test/java/com/example/MiniProject1/MiniProject1ProductTests.java
@@ -1,4 +1,25 @@
 package com.example.MiniProject1;
 
+import com.example.model.Product;
+import com.example.service.ProductService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class MiniProject1ProductTests {
+    @Autowired
+    private ProductService productService;
+
+    @Test
+    public void createProduct_withValidInput_returnsProduct() {
+        //Arrange
+        Product product = new Product("TestProduct",20);
+
+        //Act
+        Product createdProduct = productService.addProduct(product);
+
+        //Assert
+    }
 }
+
