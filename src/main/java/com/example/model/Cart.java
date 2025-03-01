085f54df5d356cfeb44bea4ecd665d67e93812b7
Mostafa El-Amory <mostafaelamory@gmail.com>
2025-03-01 13:15:02 +0200

    Added 3 constructors to each Model

--- src/main/java/com/example/model/Cart.java
+++ src/main/java/com/example/model/Cart.java
@@ -1,29 +1,18 @@
 package com.example.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-@Getter
-@Setter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Component
 public class Cart {
     private UUID id;
     private UUID userId;
     private List<Product> products=new ArrayList<>();
-
-    public Cart(UUID id, UUID userId, List<Product> products) {
-        this.id = id;
-        this.userId = userId;
-        this.products = products;
-    }
-
-    public Cart(){
-
-    }
-
 }
